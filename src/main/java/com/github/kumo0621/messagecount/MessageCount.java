c07bab4b37f9452fea334295d804173ecbc2b75e
kumo0621 <kumokumokumo0621@gmail.com>
2022-08-12 10:22:54 +0900

    チャットの回数も計測するようにした

--- src/main/java/com/github/kumo0621/messagecount/MessageCount.java
+++ src/main/java/com/github/kumo0621/messagecount/MessageCount.java
@@ -13,15 +13,24 @@ import org.jetbrains.annotations.NotNull;
 public final class MessageCount extends JavaPlugin implements org.bukkit.event.Listener {
 
     private Objective score;
+    private Objective score2;
 
     @Override
     public void onEnable() {
         Scoreboard sb = getServer().getScoreboardManager().getMainScoreboard();
         Objective sc = sb.getObjective("chatcount");
-        if (sc == null)
+        Objective sa = sb.getObjective("chattotal");
+        if (sc == null) {
             sc = sb.registerNewObjective("chatcount", "dummy", "合計チャット文字数");
+        }
+        if (sa == null) {
+            sa = sb.registerNewObjective("chattotal", "dummy", "合計チャット回数数");
+        }
         sc.setDisplaySlot(DisplaySlot.BELOW_NAME);
+        sa.setDisplaySlot(DisplaySlot.PLAYER_LIST);
         score = sc;
+        score2 = sa;
+
         getServer().getPluginManager().registerEvents(this, this);
     }
 
@@ -32,27 +41,24 @@ public final class MessageCount extends JavaPlugin implements org.bukkit.event.L
 
     @EventHandler
     public void onPlayerchat(AsyncPlayerChatEvent e) {
-        Bukkit.getScheduler().runTask(this, new Runnable() {
-            @Override
-            public void run() {
+        Bukkit.getScheduler().runTask(this, () -> {
 
-                if (count) {
-                    Player player = e.getPlayer();
-                    String chat;
-                    chat = e.getMessage();
-                    System.out.println(chat);
-                    Score sc = score.getScore(e.getPlayer().getName());
-                    sc.setScore(sc.getScore() + chat.length());
-
-
-                    }
+            if (count) {
+                Player player = e.getPlayer();
+                String chat;
+                chat = e.getMessage();
+                System.out.println(chat);
+                Score sc = score.getScore(e.getPlayer().getName());
+                Score sa = score2.getScore(e.getPlayer().getName());
+                sc.setScore(sc.getScore() + chat.length());
+//++でやるチャット回数を計測
+                int a=1;
+                sa.setScore(sa.getScore()+a);
                 }
-
-        });
+            });
     }
 
     boolean count;
-    boolean hyouzi;
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
