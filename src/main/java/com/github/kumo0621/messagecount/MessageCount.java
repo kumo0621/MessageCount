f433092788d4a9ce3b233b92470a3030c8422799
kumo0621 <kumokumokumo0621@gmail.com>
2022-08-19 18:11:43 +0900

    wwwの数を計測可能にした

--- src/main/java/com/github/kumo0621/messagecount/MessageCount.java
+++ src/main/java/com/github/kumo0621/messagecount/MessageCount.java
@@ -10,24 +10,33 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.*;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public final class MessageCount extends JavaPlugin implements org.bukkit.event.Listener {
 
     private Objective score;
     private Objective score2;
+    private Objective score3;
 
     @Override
     public void onEnable() {
         Scoreboard sb = getServer().getScoreboardManager().getMainScoreboard();
         Objective sc = sb.getObjective("chatcount");
         Objective sa = sb.getObjective("chattotal");
+        Objective s1 = sb.getObjective("wwwchat");
         if (sc == null) {
             sc = sb.registerNewObjective("chatcount", "dummy", "合計チャット文字数");
         }
         if (sa == null) {
-            sa = sb.registerNewObjective("chattotal", "dummy", "合計チャット回数数");
+            sa = sb.registerNewObjective("chattotal", "dummy", "合計チャット回数");
+        }
+        if (s1 == null) {
+            s1 = sb.registerNewObjective("wwwchat", "dummy", "合計wを打った回数");
         }
         score = sc;
         score2 = sa;
+        score3 = s1;
 
         getServer().getPluginManager().registerEvents(this, this);
     }
@@ -46,14 +55,23 @@ public final class MessageCount extends JavaPlugin implements org.bukkit.event.L
             System.out.println(chat);
             Score sc = score.getScore(e.getPlayer().getName());
             Score sa = score2.getScore(e.getPlayer().getName());
+            Score s1 = score3.getScore(e.getPlayer().getName());
             sc.setScore(sc.getScore() + chat.length());
-//++でやるチャット回数を計測
             int a = 1;
             sa.setScore(sa.getScore() + a);
-            });
+            Pattern p = Pattern.compile("w|笑|ｗ|W");
+            Matcher m = p.matcher(chat);
+            int keisoku = 0;
+            while (m.find()) {
+                keisoku++;
+            }
+            s1.setScore(s1.getScore() + keisoku);
+        });
+
     }
 
     boolean count;
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (command.getName().equals("MessageCount")) {
@@ -67,12 +85,14 @@ public final class MessageCount extends JavaPlugin implements org.bukkit.event.L
                             sender.sendMessage("チャット計測を表示にします");
                             score.setDisplaySlot(DisplaySlot.BELOW_NAME);
                             score2.setDisplaySlot(DisplaySlot.PLAYER_LIST);
+                            score3.setDisplaySlot(DisplaySlot.SIDEBAR);
                             break;
                         case "hide":
                             count = false;
                             sender.sendMessage("チャット計測を非表示にします");
                             score.setDisplaySlot(null);
                             score2.setDisplaySlot(null);
+                            score3.setDisplaySlot(null);
                             break;
                         default:
                             sender.sendMessage("不明なコマンドです。");
